f95bd6cfd8630afc60bbfaeb152a33b3604103f5
anita-tpj <anita.tpj@gmail.com>
2025-11-13 18:22:51 +0100

    Fix admin middleware

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  const isAdmin = req.user.role === "Admin";
+  const isAdmin = req.user.role === "Admin" || req.user.role === "Super Admin";
 
   if (!isAdmin) return res.status(403).send("Access denied.");
 
